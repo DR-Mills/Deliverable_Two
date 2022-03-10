0336156a8d9ef7ddb0cdc557744d6b9f52583f5d
Dustin Mills <98778591+DR-Mills@users.noreply.github.com>
2022-03-10 08:49:13 -0500

    Revert "logic implementaion"
    This reverts commit 6f7414764b7f6fc05748611ee37fb1e42fa11000.

--- src/Heads_or_Tails.java
+++ src/Heads_or_Tails.java
@@ -2,12 +2,6 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class Heads_or_Tails {
-	
-	private final static String welcomeMsg = "Welcome to the major life decision assistant. Please enter \"heads\" or \"tails\"";
-	private final static String invalidInputMsg = "Unrecognized input. Please enter either \"heads\" or \"tails\" only. Try again\n";
-	private final static String tossCountMsg = "How many coin-tosses would you like to do?";
-	private final static String startFlipMsg = "\nOk, start flipping...";
-
 	static Scanner scnr = new Scanner(System.in);
 	static Random ran = new Random();
 
@@ -15,74 +9,79 @@ public class Heads_or_Tails {
 	static int numberOfFlips = 0;
 	static int correctCount = 0;
 	static int percentCorrect = 0;
+	static boolean t = true;
 
 	public static void main(String[] args) {
-		System.out.println(welcomeMsg);
 
-		validateUserInput();
+		// welcome message. Requires correct input before proceeding
+		while (t) {
+			System.out
+					.println("Welcome to the major life decision assistant. \n" + "Please type \"heads\" or \"tails\"");
+			
+			headsOrTailsGuess = scnr.nextLine();
 
-		System.out.println(tossCountMsg);
-		numberOfFlips = scnr.nextInt(); //fix me: valid the user entry is an integer - in case of user enters a string! 
-		                                //reference link:https://stackoverflow.com/questions/3059333/validating-input-using-java-util-scanner
-		System.out.println(startFlipMsg);
+			if (isChoiceValid(headsOrTailsGuess)) {
+				t = false;
+			} else {
+				System.out.println("Unrecognized input. Please enter either \"heads\" or \"tails\" only.\n");
+			}
+		}
+		
+		System.out.println("How many coin-tosses would you like to do?");
+
+		numberOfFlips = scnr.nextInt();
+
+		System.out.println("Ok, lets start flipping...");
+		
 		totalFlips();
-
-		System.out.println("\nYour guess, " 
-				+ headsOrTailsGuess + ", came up " 
-				+ correctCount + " time(s). \nThat's "
-				+ percentCorrect(correctCount, numberOfFlips) + "%.");
-
+		
+		System.out.println("Original Guess: " + headsOrTailsGuess + ". Total Flips: " + numberOfFlips + ". Correct: "
+				+ correctCount);
+		
+		// percentCorrect();
+		// System.out.println("Your guess, " + headsOrTailsGuess + ", came up " +
+		// correctCount + " times. This is equal to " + percentCorrect + "%.");
 		scnr.close();
 
 	}
 
-	// validates user input. if input is invalid, prompt message to ask try again
-	private static void validateUserInput() {
-		boolean isValid = true;
-		while (isValid) {
-			headsOrTailsGuess = scnr.nextLine();
-
-			if (isChoiceValid(headsOrTailsGuess)) {
-				isValid = false;
-			} else {
-				System.out.print(invalidInputMsg);
-			}
-		}
-	}
-
 	// OPERATORS
 	// validates user entry of "heads" or "tails"
-	private static boolean isChoiceValid(String str) {
-		return str.equalsIgnoreCase("heads") || (str.equalsIgnoreCase("tails"));
+	public static boolean isChoiceValid(String str) {
+		if (str.equalsIgnoreCase("heads") || (str.equalsIgnoreCase("tails"))) {
+			return true;
+		}
+		return false;
 	}
 
 	// performs headsOrTails() as many times as user indicates via numberOfFlips
 	// variable and increments counter for correct guesses
-	private static void totalFlips() {
+	public static void totalFlips() {
 		for (int i = 0; i < numberOfFlips; i++) {
 			String s = headsOrTails();
-			System.out.println(s); // output each flip result
+			System.out.println(s);
 			if (s.equalsIgnoreCase(headsOrTailsGuess)) {
-				correctCount++; // increment the counter
+				correctCount++;
 			}
 		}
 	}
 
 	// Returns String "heads" or "tails" based on randomly generated 0 or 1
-	private static String headsOrTails() {
+	public static String headsOrTails() {
 		int num = ran.nextInt(2);
 		String coin = "";
 		if (num == 0) {
 			coin = "tails";
-		} else if (num == 1) {
+		}
+		else if (num == 1) {
 			coin = "heads";
 		}
 		return coin;
 	}
 
 	// percent correct calculator
-	private static int percentCorrect(int a, int b) {
-		return (int) Math.round(a * 100.0 / b);
+	public static void percentCorrect() {
+		percentCorrect = correctCount / numberOfFlips * 100;
 	}
 
 }
